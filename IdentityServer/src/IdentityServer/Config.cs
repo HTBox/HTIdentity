f6f53adf72ee6a2148730ebf5f8c87e0f0366cf2
Steve Gordon <sgordon@hotmail.co.uk>
2017-02-26 17:32:12 +0000

    Adding initial definition for an allReady client

--- IdentityServer/src/IdentityServer/Config.cs
+++ IdentityServer/src/IdentityServer/Config.cs
@@ -1,11 +1,41 @@
-﻿using System;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityServer
 {
     public class Config
     {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "allReadyMvcApp",
+                    ClientName = "allReady MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    RequireConsent = false,
+
+                    ClientSecrets =
+                    {
+                        // todo - this is temporary and should be secured properly
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris           = { "http://localhost:5002/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5002" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                    },
+
+                    AllowOfflineAccess = true
+                }
+             };
+        }
     }
 }
